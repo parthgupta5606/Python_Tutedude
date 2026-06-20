58822679b22d03ba9b9f20c01dbdcb5a2336eb34
parthgupta5606 <parthgupta5606@gmail.com>
2026-06-20 14:38:56 +0530

    Assignment 2

---
+++ Assignments/Assignment_2/Task_1.py
@@ -0,0 +1,6 @@
+#This program checks whether the entered number is even or odd
+number=int(input("Enter the Number: "))
+if number%2==0:
+    print(f"{number} is an Even Number")
+else:
+    print(f"{number} is an Odd Number")
---
+++ Assignments/Assignment_2/Task_2.py
@@ -0,0 +1,5 @@
+#This program finds out the sum of integers 1 to 50
+sum=0
+for i in range(1,51):
+    sum+=i
+print(f"The sum of numbers from 1 to 50 is: {sum}")
